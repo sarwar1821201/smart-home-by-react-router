72e10e1a4fde97f006150ac350d0d3026b755ebb
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-13 00:12:50 +0600

    send each data on the shop card routes

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ProductsCard from './ProductsCard';
 
 const Shop = () => {
 
    const productData= useLoaderData();
-   console.log(productData);
+//    console.log(productData);
 
     return (
-        <div>
+        <div className='product-container'>
+
+            {
+                productData.map(product =>  <ProductsCard  
+                 
+                key={product.id}
+                product={product}
+                >
+                </ProductsCard> 
+                )}
             
         </div>
     );
